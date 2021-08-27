a9e085b59367c570b05b4af1403bb31c36fd1242
Sagar Mopagar <sagar.mopagar@sap.com>
2021-08-27 17:46:20 +0530

    Update ValidatorTests.java
    m

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -43,7 +43,7 @@ class ValidatorTests {
 	@Test
 	void shouldNotValidateWhenFirstNameEmpty() {
 
-		//Comments ..
+		//Comments ....
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
 		person.setFirstName("");
