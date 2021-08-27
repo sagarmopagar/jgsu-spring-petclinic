4ae7e25c884905338d0dd3fc3bcc732c5350e363
Sagar Mopagar <sagar.mopagar@sap.com>
2021-08-27 17:53:27 +0530

    Update ValidatorTests.java
    d

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -29,8 +29,9 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * @author Michael Isvy Simple test to make sure that Bean Validation is working (useful
- * when upgrading to a new version of Hibernate Validator/ Bean Validation)
+ * @author Michael Isvy Simple test to make sure that Bean Validation is working
+ *         (useful when upgrading to a new version of Hibernate Validator/ Bean
+ *         Validation)
  */
 class ValidatorTests {
 
@@ -42,8 +43,7 @@ class ValidatorTests {
 
 	@Test
 	void shouldNotValidateWhenFirstNameEmpty() {
-
-	
+		//d
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
 		person.setFirstName("");
