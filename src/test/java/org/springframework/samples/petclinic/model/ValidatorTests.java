022241e731772fb36d4bd738aa334d67906fa741
Sagar Mopagar <sagar.mopagar@sap.com>
2021-09-11 22:42:34 +0530

    test

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -47,7 +47,7 @@ class ValidatorTests {
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
 		person.setFirstName("");
-//		person.setFirstName("wrong");
+//		person.setFirstName("wrong"); //
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
